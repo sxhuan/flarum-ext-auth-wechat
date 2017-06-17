839fe6e56aa52069e05cccb59e46132eb720947b
StanleySong <sxhuan@gmail.com>
2017-06-17 12:46:51 -0700

    beta finish

--- src/WechatAuthController.php
+++ src/WechatAuthController.php
@@ -9,6 +9,8 @@ use Flarum\Forum\AuthenticationResponseFactory;
 use Flarum\Forum\Controller\AbstractOAuth2Controller;
 use Flarum\Settings\SettingsRepositoryInterface;
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Zend\Diactoros\Response\RedirectResponse;
 use Henter\WeChat\OAuth;
 
 class WechatAuthController extends AbstractOAuth2Controller
@@ -28,6 +30,51 @@ class WechatAuthController extends AbstractOAuth2Controller
         $this->authResponse = $authResponse;
     }
 
+
+    /**
+     * override
+     * @param Request $request
+     * @return \Psr\Http\Message\ResponseInterface|RedirectResponse
+     */
+    public function handle(Request $request)
+    {
+        $callback_url = $this->settings->get('stanleysong-auth-wechat.callback_url');
+        $this->provider = $this->getProvider($callback_url);
+        $session = $request->getAttribute('session');
+        $code = $_GET['code'];
+        if (!isset($_GET['code'])) {
+            $url = $this->provider->getAuthorizeURL($callback_url);
+            $_SESSION['oauth2state'] = $this->generateRandomString();
+            $session->set('oauth2state', $_SESSION['oauth2state']);
+
+            return new RedirectResponse($url.'&state='.$_SESSION['oauth2state'].'&display=popup');
+        }  elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+            $session->forget('oauth2state');
+            if (isset($_SESSION['oauth2state'])) {
+                unset($_SESSION['oauth2state']);
+            }   
+            exit('Invalid state. Please close the window and try again.');
+        }  else {
+            /* wait redirect back */
+            if($access_token = $this->provider->getAccessToken('code', $code)){
+                $refresh_token = $this->provider->getRefreshToken();
+                $expires_in = $this->provider->getExpiresIn();
+                $openid = $this->provider->getOpenid();
+                $access_token = $this->provider->refreshAccessToken($refresh_token);
+
+                $this->provider = new OAuth($appid, $appkey, $access_token);
+                $userinfo = $this->provider->api('sns/userinfo', array('openid'=>$openid));
+            }else{
+                echo $this->provider->error();
+            }
+        }
+
+        $identification = $this->getId($userinfo);
+        $suggestions = $this->getSuggestion($userinfo);
+
+        return $this->authResponse->make($request, $identification, $suggestions);
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -35,34 +82,7 @@ class WechatAuthController extends AbstractOAuth2Controller
     {
         $appid = $this->settings->get('stanleysong-auth-wechat.app_id');
         $appkey = $this->settings->get('stanleysong-auth-wechat.app_secret');
-        $callback_url = $this->settings->get('stanleysong-auth-wechat.callback_url');
-
-        file_put_contents("php.log", "appid: "."\n".print_r($this->settings->get('stanleysong-auth-wechat.app_id'), true)."\n", FILE_APPEND);
-        file_put_contents("php.log", "appkey: "."\n".print_r($this->settings->get('stanleysong-auth-wechat.app_secret'), true)."\n", FILE_APPEND);
-        file_put_contents("php.log", "callback: "."\n".print_r($this->settings->get('stanleysong-auth-wechat.callback_url'), true)."\n", FILE_APPEND);
-
-        $code = $_GET['code'];
-        $oauth = new OAuth($appid, $appkey);
-        $url = $oauth->getWeChatAuthorizeURL($callback_url);
-
-        file_put_contents("php.log", "url: "."\n".print_r($url, true)."\n", FILE_APPEND);
-
-        if($access_token = $oauth->getAccessToken('code', $code)){
-            $refresh_token = $oauth->getRefreshToken();
-            $expires_in = $oauth->getExpiresIn();
-            $openid = $oauth->getOpenid();
-            $access_token = $oauth->refreshAccessToken($refresh_token);
-
-            $oauth = new OAuth($appid, $appkey, $access_token);
-            $userinfo = $oauth->api('sns/userinfo', array('openid'=>$openid));
-            $username = preg_replace('/[^a-z0-9-_]/i', '', $userinfo->getNickname());
-
-        }else{
-            echo $oauth->error();
-            file_put_contents("php.log", "error: "."\n".print_r($access_token, true)."\n", FILE_APPEND);
-        }
-
-        return $userinfo;
+        return new OAuth($appid, $appkey);
     }
 
     /**
@@ -70,7 +90,7 @@ class WechatAuthController extends AbstractOAuth2Controller
      */
     protected function getAuthorizationUrlOptions()
     {
-        return ['scope' => ['email']];
+        return null;
     }
 
     /**
@@ -78,30 +98,45 @@ class WechatAuthController extends AbstractOAuth2Controller
      */
     protected function getIdentification(ResourceOwnerInterface $resourceOwner)
     {
-        return [
-            'email' => $resourceOwner->getEmail() ?: $this->getEmailFromApi()
-        ];
+        return null;
     }
+
     /**
      * {@inheritdoc}
      */
     protected function getSuggestions(ResourceOwnerInterface $resourceOwner)
     {
+        return null;
+    }
+
+    /* customize */
+    protected function getId($userinfo)
+    {
         return [
-            'username' => $resourceOwner->getNickname(),
-            'avatarUrl' => array_get($resourceOwner->toArray(), 'avatar_url')
+            'email' => $userinfo['openid'].'@change2uremail.com'
         ];
     }
-    protected function getEmailFromApi()
+
+    protected function getSuggestion($userinfo)
     {
-        $url = $this->provider->apiDomain.'/user/emails';
-        $emails = $this->provider->getResponse(
-            $this->provider->getAuthenticatedRequest('GET', $url, $this->token)
-        );
-        foreach ($emails as $email) {
-            if ($email['primary'] && $email['verified']) {
-                return $email['email'];
-            }
+        $username = preg_replace('/[^a-z0-9-_]/i', '', $userinfo['nickname']);
+        if ($username == '')
+        {
+            $username = $userinfo['openid'];
         }
+        return [
+            'username' =>  $username,
+            'avatarUrl' => $userinfo['headimgurl']
+        ];
     }
+
+    protected function generateRandomString($length = 32) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }   
 }
